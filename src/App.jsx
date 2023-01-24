2ecccd7f7666da5c422a7a9bc751aaab12cf14cc
Keith <keithwesley187@gmail.com>
2023-01-25 02:08:27 +0300

    milestone: news ticker done

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,38 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Header from './components/Header';
 import Hero from './pages/Hero';
 import FreeGames from './pages/FreeGames';
 import MmoNews from './pages/MmoNews';
+import NewsTicker from './components/NewsTicker';
+import axios from "axios";
 
 function App() {
+
+  const [newsItems, setNewsItems] = useState(["listening for news..."]);
+
+  useEffect(() =>  {
+    const options = {
+      method: 'GET',
+      url: 'https://mmo-games.p.rapidapi.com/latestnews',
+      headers: {
+        'X-RapidAPI-Key': 'aecf993c34mshd3d18f8add32b27p113fa7jsn644341e81e9c',
+        'X-RapidAPI-Host': 'mmo-games.p.rapidapi.com'
+      }
+    };
+    
+    axios.request(options).then(function (response) {
+      setNewsItems(response.data);
+    }).catch(function (error) {
+      console.error(error);
+    });
+  }, [])
 
   return (
     <div className="App">
       <Header />
+      <NewsTicker newsItems={newsItems} />
       <Routes>
         <Route exact='true' path='/' element={<Hero />}/>
         <Route path='/free-games' element={<FreeGames />} />
--- src/components/NewsTicker.jsx
+++ src/components/NewsTicker.jsx
@@ -1,9 +1,41 @@
-import React from 'react'
+import { Box } from '@mui/material';
+import React, { useState, useRef, useEffect } from 'react';
 
-const NewsTicker = () => {
+const NewsTicker = ({newsItems}) => {
+  const [animationDistance, setAnimationDistance] = useState(0);
+  const tickerRef = useRef(null);
+
+  useEffect(() => {
+    let totalWidth = 0;
+    const tickerElements = tickerRef.current.children;
+    for (let i = 0; i < tickerElements.length; i++) {
+      totalWidth += tickerElements[i].offsetWidth;
+    }
+    setAnimationDistance(-totalWidth);
+  }, [newsItems]);
+
+  const items = [...newsItems, newsItems[0]];
+
   return (
-    <div>NewsTicker</div>
-  )
+    <Box sx={{my: 2}}>
+        <div className="wrapper">
+            <div
+                className="inner moving"
+                style={{
+                '--animationDuration': `${newsItems.length * 10}s`,
+                '--animationDistance': animationDistance + 'px'
+                }}
+                ref={tickerRef}
+            >
+                {items.map((item, index) => (
+                <div key={index} className="element">
+                    {item.title}
+                </div>
+                ))}
+            </div>
+        </div>
+    </Box>
+  );
 }
 
-export default NewsTicker
+export default NewsTicker;
