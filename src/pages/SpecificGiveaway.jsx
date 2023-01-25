b882419ef0d20f62dc6427bf64315efb5838d25a
Keith <keithwesley187@gmail.com>
2023-01-25 18:37:07 +0300

    milestone: specific giveaway done

--- src/pages/SpecificGiveaway.jsx
+++ src/pages/SpecificGiveaway.jsx
@@ -1,8 +1,9 @@
-import { Box, Grid, LinearProgress } from '@mui/material'
+import { Box, Grid, LinearProgress, Typography } from '@mui/material'
 import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom';
 import axios from "axios";
 import mainLogo from '../main-logo.svg';
+import DOMPurify from 'dompurify';
 
 const SpecificGiveaway = () => {
   const [isLoading, setIsLoading] = useState(true);
@@ -30,6 +31,13 @@ const SpecificGiveaway = () => {
 
   console.log(giveaway)
 
+  const date1 = new Date(giveaway.end_date);
+  const date2 = new Date();
+
+  const remaining = Math.floor((date1-date2) / 1000 / 60 / 60 / 24);
+  const instructions = DOMPurify.sanitize(giveaway.instructions).split("\r\n");
+  const listItems = instructions.map((instruction, index) => <li style={{listStyleType: "none", marginTop: "5px", marginBottom: "5px"}} key={index}>{instruction}</li>);
+
   if(isLoading === true) return (
     <LinearProgress color="success"/>
   );
@@ -39,29 +47,80 @@ const SpecificGiveaway = () => {
 
       <Grid container spacing={2} columns={12}>
         <Grid item xs={12} md={6}>
-          <Box id="box">
-            <div id='wrapper'>
-              <div className='cube-container'>
-                <div className='image-cube'>
-                  <div className='front'>
-                    <img src={giveaway.image} alt={giveaway.title}/>
-                  </div>
-                  <div className='right'>
-                    <img src={giveaway.image} alt={giveaway.title}/>
-                  </div>
-                  <div className='back'>
-                    <img height="100px" src={mainLogo} alt="main-logo"/>
-                  </div>
-                  <div className='left'>
-                    <img src={giveaway.image} alt={giveaway.title}/>
+          <Box>
+            <Box id="box">
+              <div id='wrapper'>
+                <div className='cube-container'>
+                  <div className='image-cube'>
+                    <div className='front'>
+                      <img src={giveaway.image} alt={giveaway.title}/>
+                    </div>
+                    <div className='right'>
+                      <img src={giveaway.image} alt={giveaway.title}/>
+                    </div>
+                    <div className='back'>
+                      <img height="100px" src={mainLogo} alt="main-logo"/>
+                    </div>
+                    <div className='left'>
+                      <img src={giveaway.image} alt={giveaway.title}/>
+                    </div>
                   </div>
                 </div>
               </div>
-            </div>
+            </Box>
           </Box>
+          
         </Grid>
-        <Grid item xs={12} md={6}>
 
+        <Grid item xs={12} md={6}>
+          <Box sx={{position: {xs: "absolute", md: "relative"}, top: {xs: "40%", md: "0%"}}}>
+            <Typography sx={{fontFamily: 'cinzel', fontSize: "25px", fontWeight: "bolder", textAlign: 'center'}} >{giveaway.title}</Typography>
+          </Box>
+          <Grid container spacing={2} columns={6}>
+            <Grid item xs={6} md={3}>
+              <Box sx={{position: {xs: "absolute", md: "relative"}, top: {xs: "50%", md: "0%"}}}>
+                <Typography sx={{fontFamily: 'Manrope', my: 2, fontSize: "16px", fontWeight: "bold", textAlign: 'start'}}>
+                  <span className='giveaway-span'>Platforms:</span> {giveaway.platforms}
+                </Typography>
+                <Typography sx={{fontFamily: 'Manrope', my: 2, fontSize: "16px", fontWeight: "bold", textAlign: 'start'}}>
+                  <span className='giveaway-span'>Status:</span> {giveaway.status}
+                </Typography>
+                <Typography sx={{fontFamily: 'Manrope', my: 2, fontSize: "16px", fontWeight: "bold", textAlign: 'start'}}>
+                  <span className='giveaway-span'>Days Remaining:</span> {date1 > 0 ? remaining + " day(s)" : "Ended"}
+                </Typography>
+              </Box>
+            </Grid>
+            <Grid item xs={6} md={3}>
+              <Box sx={{position: {xs: "absolute", md: "relative"}, top: {xs: "64%", md: "0%"}}}>
+                <Typography sx={{fontFamily: 'Manrope', my: 2, fontSize: "16px", fontWeight: "bold", textAlign: 'start'}}>
+                  <span className='giveaway-span'>Users:</span> {giveaway.users}
+                </Typography>
+                <Typography sx={{fontFamily: 'Manrope', my: 2, fontSize: "16px", fontWeight: "bold", textAlign: 'start'}}>
+                  <span className='giveaway-span'>Worth:</span> {giveaway.worth}
+                </Typography>
+                <Typography sx={{fontFamily: 'Manrope', my: 2, fontSize: "16px", fontWeight: "bold", textAlign: 'start'}}>
+                  <span className='giveaway-span'>GamePower website:</span> <a style={{cursor: "pointer", color: "#fff"}} href={giveaway.gamerpower_url} target="_blank">Link to the Page</a>
+                </Typography>
+              </Box>
+            </Grid>
+          </Grid>
+          
+        </Grid>
+
+      </Grid>
+
+      <Grid container spacing={2} columns={12}>
+        <Grid item xs={12} md={12}>
+          <Box sx={{position: {xs: "absolute", md: "relative"}, top: {xs: "80%", md: "0%"}}}>
+            <Typography 
+            sx={{fontFamily: 'cinzel', my: 2, ml: {xs: 0, md: 2}, fontSize: "25px", fontWeight: "bolder", textAlign: 'start'}}
+            >
+              How to Get your Key:
+            </Typography>
+            <Box sx={{fontFamily: 'Manrope', my: 2, fontWeight: "bold", ml: {xs: 0, md: 2}, fontSize: "16px",}}>
+              {listItems}
+            </Box>
+          </Box>
         </Grid>
       </Grid>
 
